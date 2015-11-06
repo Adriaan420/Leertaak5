8e764d38621de7858884111cec255cad55c4c772
Tim Mein <mebusmein@gmail.com>
2015-11-06 15:38:46 +0100

    fix db conneciton

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,9 +6,8 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host=localhost;dbname=fuel_db',
+			'username'   => 'root'
 		),
 	),
 );
