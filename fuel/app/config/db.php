5d509fa40197ee6aa265497e643173aaf08e8802
Tim Mein <mebusmein@gmail.com>
2015-11-06 15:31:08 +0100

    add production db config

---
+++ fuel/app/config/db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Use this file to override global defaults.
+ *
+ * See the individual environment DB configs for specific config information.
+ */
+
+return array(
+    'production' => array(
+        'connection'  => array(
+            'dsn'        => 'mysql:host=localhost;dbname=fuel_db',
+            'username'   => 'root'
+        )
+    )
+);
